d7eaaf425a8198f330cc4d708b95a34cfbcdb498
bhavyasaigatta-creator <bhavyasaigatta@gmail.com>
2026-03-18 21:15:34 +0530

    Update Number_Guessing_Game.py

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -1,7 +1,7 @@
 import random
 
 while True:
-    print("\n🎯 Number Guessing Game")
+    print("\n Number Guessing Game")
     
     number = random.randint(1, 100)
     attempts = 0
@@ -10,19 +10,19 @@ while True:
         guess = input("Enter your guess (1-100): ")
 
         if not guess.isdigit():
-            print("⚠️ Invalid input! Enter a number.")
+            print(" Invalid input! Enter a number.")
             continue
 
         guess = int(guess)
         attempts += 1
 
         if guess == number:
-            print(f"🎉 Correct! You guessed it in {attempts} attempts.")
+            print(f" Correct! You guessed it in {attempts} attempts.")
             break
         elif guess < number:
-            print("📉 Too low!")
+            print(" Too low!")
         else:
-            print("📈 Too high!")
+            print(" Too high!")
 
     # Ask to play again
     choice = input("Do you want to play again? (yes/no): ").lower()
